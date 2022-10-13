be3ccb7d6123c9bf68d0bd7c774256fcd6ed7606
Derick Hermanson <derick.hermanson@gmail.com>
2022-10-12 19:49:25 -0500

    initial commit

---
+++ src/main/kotlin/com/dt2js/dgs/DgsFrameworkDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.dt2js.dgs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DgsFrameworkDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DgsFrameworkDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/dt2js/dgs/DgsFrameworkDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dt2js.dgs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DgsFrameworkDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
