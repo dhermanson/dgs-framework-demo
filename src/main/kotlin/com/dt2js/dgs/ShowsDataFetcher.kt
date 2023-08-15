7aa5ca81d75cb5dac12ee26ed568a4c22c70aa1b
Derick Hermanson <derick.hermanson@gmail.com>
2023-08-15 17:14:27 -0500

    update dependencies and add suspend to data fetcher query

--- src/main/kotlin/com/dt2js/dgs/ShowsDataFetcher.kt
+++ src/main/kotlin/com/dt2js/dgs/ShowsDataFetcher.kt
@@ -4,9 +4,15 @@ import com.dt2js.dgs.types.Show
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsQuery
 import com.netflix.graphql.dgs.InputArgument
+import org.slf4j.LoggerFactory
 
 @DgsComponent
 class ShowsDataFetcher {
+
+    companion object {
+        val LOG = LoggerFactory.getLogger(ShowsDataFetcher::class.java)
+    }
+
     private val shows = listOf(
         Show("Stranger Things", 2016),
         Show("Ozark", 2017),
@@ -15,7 +21,9 @@ class ShowsDataFetcher {
         Show("Orange is the New Black", 2013))
 
     @DgsQuery
-    fun shows(@InputArgument titleFilter : String?): List<Show> {
+    suspend fun shows(@InputArgument titleFilter : String?): List<Show> {
+
+        LOG.info("here")
         return if(titleFilter != null) {
             shows.filter { it.title.contains(titleFilter) }
         } else {
